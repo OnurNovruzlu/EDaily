be3b416fde65b29c3e96f6da38e7257dfd7ff833
OnurNovruzlu <onur.novruzlu@mail.ru>
2023-07-21 10:32:22 +0400

    edaily project

---
+++ src/main/java/az/coftea/edaily/EDailyApplication.java
@@ -0,0 +1,13 @@
+package az.coftea.edaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/coftea/edaily/EDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.coftea.edaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
